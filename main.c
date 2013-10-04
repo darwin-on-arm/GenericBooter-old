bb443ad20e2ae3fb1c94ab62e6817400dfd04de8
winocm <winocm@icloud.com>
2013-10-03 21:07:02 -0500

    Revert "OMAP3530 version"
    This reverts commit 0cd074b27090b0ad46450b0d9294f8d38f8085ce.

--- main.c
+++ main.c
@@ -108,7 +108,6 @@ corestart_main(uint32_t __unused, uint32_t machine_type, struct atag *atags)
     /* We're in. */
     init_debug();
 
-#if 0
     /*
      * Verify machine type.
      */
@@ -122,14 +121,13 @@ corestart_main(uint32_t __unused, uint32_t machine_type, struct atag *atags)
                MACH_TYPE_REALVIEW_PBA8);
         _locore_halt_system();
     }
-#endif
 
     /*
      * Announce ourselves.
      */
     printf("=======================================\n"
            "::\n"
-           ":: GenericBooter for OMAP3530, Copyright 2013, winocm.\n"
+           ":: GenericBooter for ARM RealView, Copyright 2013, winocm.\n"
            "::\n"
            "::\tBUILD_TAG: %s\n"
            "::\n"
